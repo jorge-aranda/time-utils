bbdab7b7ad67f3a19c0d2b7643389cbb17905a9a
Jorge Aranda de Miguel <jorgearanda123@gmail.com>
2018-03-11 15:06:21 +0100

    test: Include // TODO to implement (someday) a MockMvc test into controller

--- time-range-example/src/main/java/es/jaranda/commons/timeutils/examples/timerangeexample/api/controller/DateTimeController.java
+++ time-range-example/src/main/java/es/jaranda/commons/timeutils/examples/timerangeexample/api/controller/DateTimeController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Optional;
 
+// TODO pending to include mockMvc test
 @RequiredArgsConstructor
 @RequestMapping("/current-date-time")
 @RestController
